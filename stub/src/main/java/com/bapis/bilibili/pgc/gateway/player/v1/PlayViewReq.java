9ba5ad6d2e173e5fc97e0af6e8dc5ab313a9656e
meolunr <meolunr@gmail.com>
2020-07-25 17:19:00 +0800

    Stub: Add the PlayViewReq shadow from host

---
+++ stub/src/main/java/com/bapis/bilibili/pgc/gateway/player/v1/PlayViewReq.java
@@ -0,0 +1,31 @@
+package com.bapis.bilibili.pgc.gateway.player.v1;
+
+public final class PlayViewReq {
+    public long getCid() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public long getEpId() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int getFnval() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int getFnver() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int getForceHost() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public boolean getFourk() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public long getQn() {
+        throw new RuntimeException("Stub!");
+    }
+}
