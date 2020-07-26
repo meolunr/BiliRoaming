77a897295c31e655da20d3a25c346117987047ba
meolunr <meolunr@gmail.com>
2020-07-26 14:43:08 +0800

    Stub: Optimize host shadow class name that needs to be loaded

--- app/src/main/java/me/iacn/biliroaming/inject/HybridClassLoader.java
+++ app/src/main/java/me/iacn/biliroaming/inject/HybridClassLoader.java
@@ -1,5 +1,7 @@
 package me.iacn.biliroaming.inject;
 
+import me.iacn.biliroaming.BuildConfig;
+
 /**
  * Created by Meolunr on 2020/7/24
  * Email meolunr@gmail.com
@@ -13,7 +15,10 @@ class HybridClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        if (name != null && name.startsWith("com.bapis.bilibili")) {
+        if (name != null) {
+            if (name.startsWith("kotlin.") || name.startsWith("kotlinx.") || name.startsWith(BuildConfig.APPLICATION_ID)) {
+                return super.findClass(name);
+            }
             return hostParent.loadClass(name);
         }
         return super.findClass(name);
