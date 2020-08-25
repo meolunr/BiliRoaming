44c626701add4388fb36a9afb4262030bf75d326
meolunr <meolunr@gmail.com>
2020-08-25 17:27:39 +0800

    Fix colorful EditText not working on Android O+

--- app/src/main/java/me/iacn/biliroaming/ColorChooseDialog.kt
+++ app/src/main/java/me/iacn/biliroaming/ColorChooseDialog.kt
@@ -13,8 +13,7 @@ import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
-import me.iacn.editcolorhelper.EditColorHelper
-
+import com.meolunr.edittextcolorhelper.EditTextColorHelper
 
 /**
  * Created by iAcn on 2019/7/14
@@ -117,7 +116,7 @@ class ColorChooseDialog(context: Context, defColor: Int) : AlertDialog.Builder(c
 
         updateSeekBarColor(color)
         updateButtonColor(color)
-        EditColorHelper.setColor(etColor, color)
+        EditTextColorHelper.setColor(etColor, color)
     }
 
     private fun updateSeekBarColor(color: Int) {
